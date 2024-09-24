4c869e489429ae40149a70d491ef780670882bf4
vien303cat <vien303cat@gmail.com>
2024-09-24 13:49:44 +0800

    update

---
+++ 20240920_JS/jsdoc.js
@@ -0,0 +1,34 @@
+/**
+ * 早安
+ */
+const GoodMorning = () => {}
+GoodMorning()
+
+/**
+ * 早安
+ * @param time 時間
+ * @param item 物品
+ */
+const GoodMorning2 = (time, item) => {}
+
+/**
+ * 早安
+ * @param time {string} 時間
+ * @param item {string} 物品
+ */
+const GoodMorning3 = (time, item) => {}
+
+/**
+ * 早安
+ * @param [time=早上] {string} 時間
+ * @param [item=冰淇淋] {string} 物品
+ */
+const GoodMorning4 = (time = '早上', item = '冰淇淋') => {}
+
+/**
+ * 早安
+ * @param [time=早上] {string} 時間
+ * @param [item=冰淇淋] {string} 物品
+ * @returns {string} 組合完的訊息
+ */
+const GoodMorning5 = (time = '早上', item = '冰淇淋') => {}
